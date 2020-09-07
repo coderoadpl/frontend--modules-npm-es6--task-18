a7c27f1fb00e27d0e8877d824af18ae771099da3
Mateusz Choma <kontakt@coderoad.pl>
2020-09-07 07:39:09 +0200

    Initialized from task-17

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const string1 = 'Ala ma kota'
+const string2 = 'Ala\nma kota'
+// multiline strings
+const string3 = (
+    `Ala
+    ma kota`
+)
+
+console.log(string1)
+console.log(string2)
+console.log(string3)
+
+const name = 'Ola'
+const animal = 'psa'
+
+const string4 = name + ' ma ' + animal
+const string5 = `${name} ma ${animal}`
+
+console.log(string4)
+console.log(string5)
+
+const string6 = `The result of 2 + 2 is ${2 + 2}`
+
+console.log(string6)
